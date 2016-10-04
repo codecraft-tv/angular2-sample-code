27afacc6c2b9a67c248e4ded5b56bb5cb18d7cbb
Asim Hussain <jawache@gmail.com>
2016-10-04 17:19:45 +0100

    Completed 8

---
+++ 8.pipes/2.built-in-pipes/script.ts
@@ -0,0 +1,155 @@
+import {NgModule, Component, Pipe} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {Observable} from 'rxjs/Rx';
+
+@Component({
+  selector: 'pipe-builtins',
+  template: `<div class="card card-block">
+  <h4 class="card-title">Currency</h4>
+  <div class="card-text">
+    <p ngNonBindable>{{ 1234.56 | currency:'GBP' }}</p>
+    <p>{{ 1234.56 | currency:"GBP" }}</p>
+    
+    <p ngNonBindable>{{ 1234.56 | currency:'GBP':'true' }}</p>
+    <p>{{ 1234.56 | currency:"GBP":true }}</p>
+  </div>
+</div>
+
+<div class="card card-block">
+  <h4 class="card-title">Date</h4>
+  <div class="card-text">
+    <p ngNonBindable>{{ dateVal | date: 'shortTime' }}</p>
+    <p>{{ dateVal | date: 'shortTime' }}</p>
+
+    <p ngNonBindable>{{ dateVal | date:'fullDate' }}</p>
+    <p>{{ dateVal | date: 'fullDate' }}</p>
+
+    <p ngNonBindable>{{ dateVal | date: 'shortTime' }}</p>
+    <p>{{ dateVal | date: 'shortTime' }}</p>
+
+    <p ngNonBindable>{{ dateVal | date: 'd/M/y' }}</p>
+    <p>{{ dateVal | date: 'd/M/y' }}</p>
+  </div>
+</div>
+
+<div class="card card-block">
+  <div class="card-text">
+    <h4 class="card-title">DecimalPipe</h4>
+    <p ngNonBindable>{{ decimalVal | number: '3.1-2' }}</p>
+    <p>{{ decimalVal | number: '3.1-2' }}</p>
+
+    <p ngNonBindable>{{ decimalVal | number: '1.4-4' }}</p>
+    <p>{{ decimalVal | number: '1.4-4' }}</p>
+  </div>
+
+</div>
+
+<div class="card card-block">
+  <h4 class="card-title">JsonPipe</h4>
+  <div class="card-text">
+    <p ngNonBindable>{{ jsonVal }}</p>
+    <p>{{ jsonVal }}</p>
+    
+    <p ngNonBindable>{{ jsonVal | json }}</p>
+    <p>{{ jsonVal | json }}</p>
+  </div>
+</div>
+
+
+<div class="card card-block">
+  <h4 class="card-title">LowerCasePipe</h4>
+  <div class="card-text">
+    <p ngNonBindable>{{ 'asim' | uppercase }}</p>
+    <p>{{ 'asim' | uppercase }}</p>
+  </div>
+</div>
+
+<div class="card card-block">
+  <h4 class="card-title">UpperCasePipe</h4>
+  <div class="card-text">
+    <p ngNonBindable>{{ 'asim' | uppercase }}</p>
+    <p>{{ 'asim' | uppercase }}</p>
+  </div>
+</div>
+
+<div class="card card-block">
+  <h4 class="card-title">PercentPipe</h4>
+  <div class="card-text">
+    <p ngNonBindable>{{ 0.123456 | percent }}</p>
+    <p>{{ 0.123456 | percent }}</p>
+
+    <p ngNonBindable>{{ 0.123456 | percent: '2.1-2' }}</p>
+    <p>{{ 0.123456 | percent: '2.1-2' }}</p>
+
+    <p ngNonBindable>{{ 42 | percent: '10.4-4' }}</p>
+    <p>{{ 0.123456 | percent : "10.4-4" }}</p>
+  </div>
+</div>
+
+<div class="card card-block">
+  <h4 class="card-title">SlicePipe</h4>
+  <div class="card-text">
+    <p ngNonBindable>{{ [1,2,3,4,5,6] | slice:1:3 }}</p>
+    <p>{{ [1,2,3,4,5,6] | slice:1:3 }}</p>
+
+    <p ngNonBindable>{{ [1,2,3,4,5,6] | slice:2 }}</p>
+    <p>{{ [1,2,3,4,5,6] | slice:2 }}</p>
+
+    <p ngNonBindable>{{ [1,2,3,4,5,6] | slice:2:-1 }}</p>
+    <p>{{ [1,2,3,4,5,6] | slice:2:-1 }}</p>
+
+    <pre ngNonBindable>   
+&lt;ul&gt;
+  &lt;li *ngFor=&quot;let v of [1,2,3,4,5,6] | slice:2:-1&quot;&gt;
+    {{v}}
+  &lt;/li&gt;
+&lt;/ul&gt;
+    </pre>    
+
+    <ul>
+      <li *ngFor="let v of [1,2,3,4,5,6] | slice:2:-1">
+        {{v}}
+      </li>
+    </ul>
+  </div>
+</div>
+
+ `
+})
+class PipeBuiltinsComponent {
+  private dateVal: Date = new Date();
+  private decimalVal: number = Math.PI;
+  private jsonVal: Object = {
+    moo: 'foo',
+    goo: {
+      too: 'new'
+    }
+  };
+
+}
+
+@Component({
+  selector: 'app',
+  template: `
+<pipe-builtins></pipe-builtins>
+ `
+})
+class AppComponent {
+  imageUrl: string = "";
+
+}
+
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent,
+    PipeBuiltinsComponent
+  ],
+  bootstrap: [AppComponent],
+})
+class AppModule {
+
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ 8.pipes/4.async-pipe/script.ts
@@ -0,0 +1,79 @@
+import {NgModule, Component, Pipe} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {Observable} from 'rxjs/Rx';
+
+
+@Component({
+  selector: 'async-pipe',
+  template: `
+ <div class="card card-block">
+  <h4 class="card-title">AsyncPipe</h4>
+  
+  <p class="card-text" ngNonBindable>{{ promise | async }}  </p>
+  <p class="card-text">{{ promise | async }}  </p>
+
+  <p class="card-text" ngNonBindable>{{ observable | async }}  </p>
+  <p class="card-text">{{ observable | async }}</p>
+ 
+  <p class="card-text" ngNonBindable>{{ observableData }}  </p>
+  <p class="card-text">{{ observableData }}</p> 
+ </div> 
+  `
+})
+class AsyncPipeComponent {
+  promise: Promise<string>;
+  observable: Observable<number>;
+  observableData: number;
+
+  constructor() {
+    this.promise = this.getPromise();
+    this.observable = this.getObservable();
+    this.subscribeObservable();
+
+  }
+
+  getObservable() {
+    return Observable
+        .interval(1000)
+        .take(10)
+        .map((v) => v * v);
+  }
+
+  subscribeObservable() {
+    this.getObservable()
+        .subscribe((v) => this.observableData = v);
+  }
+
+  getPromise() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve("Promise complete!"), 3000);
+    });
+  }
+}
+
+
+@Component({
+  selector: 'app',
+  template: `
+  <async-pipe></async-pipe>
+ `
+})
+class AppComponent {
+  imageUrl: string = "";
+
+}
+
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent,
+    AsyncPipeComponent
+  ],
+  bootstrap: [AppComponent],
+})
+class AppModule {
+
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
