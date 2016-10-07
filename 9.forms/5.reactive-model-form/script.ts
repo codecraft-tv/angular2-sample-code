73102a1a72312bde919bd21e104a7aca9d27ba6c
Asim Hussain <jawache@gmail.com>
2016-10-07 17:04:23 +0100

    Completed 9.5

---
+++ 9.forms/5.reactive-model-form/script.ts
@@ -0,0 +1,69 @@
+import {
+    NgModule,
+    Component,
+    OnInit
+} from '@angular/core';
+import {
+    ReactiveFormsModule,
+    FormsModule,
+    FormGroup,
+    FormControl
+} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'reactive-model-form',
+  template: `<input type="search"
+       class="form-control"
+       placeholder="Please enter search term"
+       [formControl]="searchField">
+<hr/>
+<ul>
+  <li *ngFor="let search of searches">{{ search }}</li>
+</ul>
+`
+})
+class ReactiveModelFormComponent implements OnInit {
+
+  searchField: FormControl;
+  searches: string[] = [];
+
+  ngOnInit() {
+    this.searchField = new FormControl();
+    this.searchField.valueChanges
+        .debounceTime(400)
+        .distinctUntilChanged()
+        .subscribe(term => {
+          this.searches.push(term);
+        });
+  }
+}
+
+@Component({
+  selector: 'app',
+  template: `<reactive-model-form></reactive-model-form>`
+})
+class AppComponent {
+}
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AppComponent,
+    ReactiveModelFormComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+})
+class AppModule {
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
