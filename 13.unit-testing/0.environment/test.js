b48878ed84d33b9694ed90c1fed8cdca00ba609a
Asim Hussain <jawache@gmail.com>
2016-10-19 19:45:32 +0100

    Finished 13.2

---
+++ 13.unit-testing/0.environment/test.js
@@ -0,0 +1,17 @@
+describe('Hello world', () => {
+
+  let expected = "";
+
+  beforeEach(() => {
+    expected = "Hello world";
+  });
+
+  afterEach(() => {
+    expected = "";
+  });
+
+  it('says hello', () => {
+    expect(helloWorld())
+        .toEqual(expected);
+  });
+});
