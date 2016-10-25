0921056eec3387f1f6dbe89269c17f069c473ba8
Asim Hussain <jawache@gmail.com>
2016-10-25 12:21:56 +0100

    Formatted section 1

--- 1.quickstart/4.interp/script.ts
+++ 1.quickstart/4.interp/script.ts
@@ -4,26 +4,26 @@ import {BrowserModule} from '@angular/platform-browser';
 import {Component} from '@angular/core';
 
 @Component({
-	selector: 'joke',
-	template: `
+  selector: 'joke',
+  template: `
     <h1>{{setup}}</h1>
     <p>{{punchline}}</p>
   `
 })
 class JokeComponent {
-	setup: string;
-	punchline: string;
+  setup: string;
+  punchline: string;
 
-	constructor() {
-		this.setup = "What did the cheese say when it looked in the mirror?";
-		this.punchline = "Halloumi (Hello Me)";
-	}
+  constructor() {
+    this.setup = "What did the cheese say when it looked in the mirror?";
+    this.punchline = "Halloumi (Hello Me)";
+  }
 }
 
 @NgModule({
-	imports: [BrowserModule],
-	declarations: [JokeComponent],
-	bootstrap: [JokeComponent]
+  imports: [BrowserModule],
+  declarations: [JokeComponent],
+  bootstrap: [JokeComponent]
 })
 export class AppModule {
 }
