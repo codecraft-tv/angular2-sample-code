27275065ed14dc289af6723628b6f11321a35eab
Asim Hussain <jawache@gmail.com>
2016-11-26 18:40:17 +0000

    Activities for forms

--- 9.forms/@solution/script.ts
+++ 9.forms/@solution/script.ts
@@ -38,21 +38,39 @@ class Joke {
   selector: 'joke-form',
   template: `<div class="card card-block">
   <h4 class="card-title">Create Joke</h4>
-  <form>
-    <div class="form-group">
+  <form novalidate
+        [formGroup]="form">
+    <div class="form-group"
+         [ngClass]="{
+        'has-danger': setup.invalid && (setup.dirty || setup.touched),
+        'has-success': setup.valid && (setup.dirty || setup.touched)
+      }">
       <input type="text"
              class="form-control"
              placeholder="Enter the setup"
-             #setup>
+             formControlName="setup">
+      <div class="form-control-feedback"
+           *ngIf="setup.errors && (setup.dirty || setup.touched)">
+        <p *ngIf="setup.errors.required">Setup is required</p>
+      </div>
     </div>
-    <div class="form-group">
+    <div class="form-group"
+         [ngClass]="{
+        'has-danger': punchline.invalid && (punchline.dirty || punchline.touched),
+        'has-success': punchline.valid && (punchline.dirty || punchline.touched)
+      }">
       <input type="text"
              class="form-control"
              placeholder="Enter the punchline"
-             #punchline>
+             formControlName="punchline">
+      <div class="form-control-feedback"
+           *ngIf="punchline.errors && (punchline.dirty || punchline.touched)">
+        <p *ngIf="punchline.errors.required">Punchline is required</p>
+      </div>
     </div>
     <button type="button"
             class="btn btn-primary"
+            [disabled]="form.invalid"
             (click)="createJoke(setup.value, punchline.value)">Create
     </button>
   </form>
@@ -73,11 +91,15 @@ class JokeFormComponent {
 
 
   createFormControls() {
-    //TODO
+    this.setup = new FormControl('', Validators.required);
+    this.punchline = new FormControl('', Validators.required);
   }
 
   createForm() {
-    //TODO
+    this.form = new FormGroup({
+      punchline: this.punchline,
+      setup: this.setup
+    });
   }
 
   createJoke(setup: string, punchline: string) {
@@ -157,7 +179,8 @@ class AppComponent {
 @NgModule({
   imports: [
     BrowserModule,
-    //TODO - make sure to import and add the required modules!
+    ReactiveFormsModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
