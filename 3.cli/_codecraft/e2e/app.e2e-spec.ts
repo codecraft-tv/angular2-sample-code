4891976de2829a8fd81915a23646f4f8746d02f6
jawache <jawache@gmail..com>
2017-12-19 14:30:16 +0000

    Updated to Angular 5

--- 3.cli/_codecraft/e2e/app.e2e-spec.ts
+++ 3.cli/_codecraft/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('activity App', () => {
+describe('codecraft App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- 3.cli/codecraft/src/app/app.module.ts
+++ 3.cli/codecraft/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+import { JokeListComponent } from './joke-list/joke-list.component';
+import { JokeFormComponent } from './joke-form/joke-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    JokeListComponent,
+    JokeFormComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
