61dc23b1c255f7c84599ce525dc91fa58c1d08f3
jawache <jawache@gmail..com>
2018-01-16 12:56:03 +0000

    Updated RxJS Version

---
+++ upload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd '1.quickstart' && plkall & cd ..
+cd '2.es6-typescript' && plkall & cd ..
+# '3.cli',
+# '4.components',
+# '5.built-in-directives',
+# '6.custom-directives',
+# '7.reactive-programming-with-rxjs',
+# '8.pipes',
+# '9.forms',
+# '10.dependency-injection-and-providers',
+# '11.http',
+# '12.routing',
+# '13.unit-testing',
+# '14.advanced-topics/1.custom-validators',
+# '14.advanced-topics/2.configurable-custom-validators'
+
+# echo $FOLDERS
+
+# for f in $FOLDERS
+# do
+#     echo $f
+# done
