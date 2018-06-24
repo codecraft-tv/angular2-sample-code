14c4ca5fc49608a3bf1f17795dd05e480628f356
jawache <jawache@gmail..com>
2018-06-24 16:46:50 +0100

    Updated DI/tokens to not use reflective injector

--- 10.dependency-injection-and-providers/4.tokens/script.ts
+++ 10.dependency-injection-and-providers/4.tokens/script.ts
@@ -1,44 +1,41 @@
-import {ReflectiveInjector} from '@angular/core';
-import {InjectionToken} from '@angular/core';
+import { Injector } from "@angular/core";
+import { InjectionToken } from "@angular/core";
 
-// String Token (Fail Case) Example
 {
+  // String Token (Fail Case) Example
   console.log("String Token (Fail Case) Example");
-  class MandrillService {
-  }
-  class SendGridService {
-  }
+  class MandrillService {}
+  class SendGridService {}
 
+  // If using strings we can cause a name clash, like so, two tokens, same string.
   let MandrillServiceToken = "EmailService";
   let SendGridServiceToken = "EmailService";
 
-  let injector = ReflectiveInjector.resolveAndCreate([
-    {provide: SendGridServiceToken, useClass: SendGridService},
-    {provide: MandrillServiceToken, useClass: MandrillService},
+  let injector = Injector.create([
+    { provide: SendGridServiceToken, useClass: SendGridService, deps: [] },
+    { provide: MandrillServiceToken, useClass: MandrillService, deps: [] }
   ]);
 
   let emailService1 = injector.get(SendGridServiceToken);
   let emailService2 = injector.get(MandrillServiceToken);
-  console.log(emailService1 === emailService2);
+  console.log(emailService1 === emailService2); // true
 }
 
-// OpaqueToken
+// InjectionToken
 {
   console.log("InjectionToken");
-  class MandrillService {
-  }
-  class SendGridService {
-  }
+  class MandrillService {}
+  class SendGridService {}
 
   const MandrillServiceToken = new InjectionToken<string>("EmailService");
   const SendGridServiceToken = new InjectionToken<string>("EmailService");
 
-  let injector = ReflectiveInjector.resolveAndCreate([
-    {provide: SendGridServiceToken, useClass: SendGridService},
-    {provide: MandrillServiceToken, useClass: MandrillService},
+  let injector = Injector.create([
+    { provide: SendGridServiceToken, useClass: SendGridService, deps: [] },
+    { provide: MandrillServiceToken, useClass: MandrillService, deps: [] }
   ]);
 
   let emailService1 = injector.get(SendGridServiceToken);
   let emailService2 = injector.get(MandrillServiceToken);
-  console.log(emailService1 === emailService2);  // false
+  console.log(emailService1 === emailService2); // false
 }
